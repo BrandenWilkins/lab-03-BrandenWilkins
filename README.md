da6dad589bc9663d07d994795672e15ea6d9bd99
BrandenWilkins <bwilki20@uwyo.edu>
2024-09-25 00:33:13 +0000

    I finished

--- README.md
+++ README.md
@@ -1,6 +1,6 @@
-# [Name]
-## [Assignment #]
-## [Submission date: MM/DD/YY]
+# [Branden Wilkins]
+## [Lab 3]
+## [Submission date: 09/24/24]
 ## Worked with/sources 
 * example
 * example
---
+++ practice.py
@@ -0,0 +1,9 @@
+names = ["Branden", "Brian", "Yousef", "Meshref"]
+print(names)
+
+del names[0]
+print(names)
+names.append("ben")
+print(names)
+for name in names:
+    print(f"{name} is at my table" )
